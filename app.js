ec6582d72ec24afc72264d8d395b58c86877952e
shivam <mailtoshivam2002@gmail.com>
2023-02-11 17:58:32 +0530

    working with router bodyparser task 2.3

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const http = require("http");
-
 const express = require("express");
+
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middlerware");
-  next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/proudct" method="POST"><input placeholder="product" type="text" name="title"><input placeholder="size" type="text" name="size"><button type="submit">Add Product</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In another middlerware");
-  res.send('<h1>Hellow from Express</h1>');
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-// const server = http.createServer(app);
-
-// server.listen(3000);
-
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hellow from  Express</h1>");
+});
 
 app.listen(3000);
